61a7203722f741a25a96c2e6f77d91213b4e6c03
Philip Newborough <corenominal@corenominal.org>
2016-01-25 21:02:19 +0000

    Add settings link

--- iewp-opengraph-n-twittercards.php
+++ iewp-opengraph-n-twittercards.php
@@ -10,3 +10,22 @@
 
 require_once( plugin_dir_path( __FILE__ ) . 'metadata.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'admin-page.php' );
+
+
+function iewp_metadata_action_links( $actions, $plugin_file ) 
+{
+	static $plugin;
+
+	if (!isset($plugin))
+		$plugin = plugin_basename(__FILE__);
+	if ($plugin == $plugin_file)
+	{
+		$settings = array('settings' => '<a href="options-general.php?page=iewp-metadata">' . __('Settings', 'General') . '</a>');
+		//$site_link = array('support' => '<a href="http://corenominal.org" target="_blank">Support</a>');
+	
+		$actions = array_merge($settings, $actions);
+		//$actions = array_merge($site_link, $actions);	
+	}
+	return $actions;
+}
+add_filter( 'plugin_action_links', 'iewp_metadata_action_links', 10, 5 );
